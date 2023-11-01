67000d3602307d3118b7c7a22d7eae4961927b18
Mahammad Irshad ap <irshadalike10@gmail.com>
2023-11-01 10:53:12 +0530

    updation

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 mongoose.set('strictQuery', false)
 const URL = process.env.MONGO
-// const URL = 'mongodb://127.0.0.1:27017/famms'
+
 console.log("Connection Success",URL );
 
 const DB=mongoose.connect(URL).then((data) => {                                                
@@ -12,4 +12,3 @@ const DB=mongoose.connect(URL).then((data) => {
 
 module.exports=DB
 
-// const URL = 'mongodb://127.0.0.1:27017/famms'
